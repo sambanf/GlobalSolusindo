567c7cf83aef0655dc9e84cc38af08545bdb33a9
supendi <irpan.supendi@gmail.com>
2019-03-16 19:58:32 +0700

    change default order, by its title

--- GlobalSolusindo.Web/app/modules/role/roleDtService.js
+++ GlobalSolusindo.Web/app/modules/role/roleDtService.js
@@ -24,25 +24,26 @@
                     pageIndex: 1,
                     pageSize: 10
                 },
+                order: [1, "asc"],
                 columns: [{
-                        "orderable": false,
-                        "data": "role_pk"
-                    },
-                    {
-                        "data": "title"
-                    },
-                    {
-                        "data": "description"
-                    },
-                    {
-                        "orderable": false,
-                        "className": "text-center",
-                        "render": function (data) {
-                            return "<button id='show' rel='tooltip' title='Detail' data-placement='left' class='btn btn-success'><i class='fa fa-info'></i></button> " +
-                                "<button id='view' rel='tooltip' title='Edit' data-placement='left' class='btn btn-warning'><i class='fas fa-pencil-alt'></i></button> " +
-                                "<button id='delete' rel='tooltip' title='Delete' data-placement='left' class='btn btn-danger'><i class='fa fa-trash-alt'></i></button>"
-                        }
+                    "orderable": false,
+                    "data": "role_pk"
+                },
+                {
+                    "data": "title"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "orderable": false,
+                    "className": "text-center",
+                    "render": function (data) {
+                        return "<button id='show' rel='tooltip' title='Detail' data-placement='left' class='btn btn-success'><i class='fa fa-info'></i></button> " +
+                            "<button id='view' rel='tooltip' title='Edit' data-placement='left' class='btn btn-warning'><i class='fas fa-pencil-alt'></i></button> " +
+                            "<button id='delete' rel='tooltip' title='Delete' data-placement='left' class='btn btn-danger'><i class='fa fa-trash-alt'></i></button>"
                     }
+                }
                 ]
             });
         }
