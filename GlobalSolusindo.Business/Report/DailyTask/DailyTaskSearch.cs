32aed9cb3762ba432907b1d5fa871f077c93f252
faridpermana7 <faridpermana7@gmail.com>
2019-05-19 20:20:09 +0700

    Report untuk DailyTask
    Report untuk DailyTask

--- GlobalSolusindo.Business/Report/DailyTask/DailyTaskSearch.cs
+++ GlobalSolusindo.Business/Report/DailyTask/DailyTaskSearch.cs
@@ -8,8 +8,11 @@ namespace GlobalSolusindo.Business.DailyTask.Queries
 {
     public class DailyTaskSearchFilter : SearchFilter
     {
-        [JsonProperty("userId")]
-        public string UserId { get; set; }
+        [JsonProperty("user_fk")]
+        public int User_FK { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
 
         [JsonProperty("userName")]
         public string UserName { get; set; }
@@ -35,13 +38,17 @@ namespace GlobalSolusindo.Business.DailyTask.Queries
                     )
                     .AsQueryable();
 
-            //if (!string.IsNullOrEmpty(filter.UserId))
-            //{
-            //    filteredRecords = filteredRecords
-            //        .Where(x =>
-            //            x.UserId == filter.UserId
-            //            );
-            //}
+            if (filter.User_FK > 0)
+            {
+                filteredRecords = filteredRecords
+                    .Where(x => x.User_FK == filter.User_FK);
+            }
+
+            if (!string.IsNullOrEmpty(filter.Status) && filter.Status != "ALL")
+            {
+                filteredRecords = filteredRecords
+                    .Where(x => x.Status == filter.Status  );
+            }
 
             //if (!string.IsNullOrEmpty(filter.UserName))
             //{
--- GlobalSolusindo.Web/app/modules/report/dailyTask/dailyTaskCtrl.js
+++ GlobalSolusindo.Web/app/modules/report/dailyTask/dailyTaskCtrl.js
@@ -4,13 +4,37 @@
     angular.module('global-solusindo')
         .controller('DailyTaskCtrl', DailyTaskCtrl);
 
-    DailyTaskCtrl.$inject = ['$scope', '$state', 'dailyTaskDtService'];
+    DailyTaskCtrl.$inject = ['$scope', '$state', 'dailyTaskDtService', 'HttpService'];
 
-    function DailyTaskCtrl($scope, $state, dtService) {
+    function DailyTaskCtrl($scope, $state, dtService, http) {
         var self = this;
-
+        self.formData ={};
+        self.formData.users = [
+            { user_fk:0, name: "ALL" }
+        ];
         dtService.init(self);
-
+        
+        function getUsers(jabatanFk, keyword) {
+            http.get('user/search', {
+                pageIndex: 1,
+                pageSize: 10,
+                keyword: keyword
+            }).then(function (response) {
+                response.data.records.forEach(function(item){
+                    self.formData.users.push(item);
+                });
+                console.log(self.formData.users);
+                self.formData.status = [
+                    {statusId: 0, name: "ALL"},
+                    {statusId: 1, name: "Online"},
+                    {statusId: 2, name: "Cuti"},
+                    {statusId: 3, name: "Unassigned"},
+                    {statusId: 4, name: "Offline"},
+                ]
+                // console.log(response);
+            });
+        };
+        getUsers();
         return self;
     }
 })();
