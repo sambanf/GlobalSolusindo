19200df7347d6f7ba33b7a9db9c1acc26aa01aff
Fikri Fikrul Mubarok <fikrifikrulmubarok@y7mail.com>
2019-07-05 15:17:53 +0700

    Add Dashboard
    Add Dashboard

---
+++ GlobalSolusindo.Api/Controllers/DashboardController.cs
@@ -0,0 +1,116 @@
+﻿using GlobalSolusindo.Api.Models;
+using GlobalSolusindo.Business.PO;
+using GlobalSolusindo.Business.PO.DML;
+using GlobalSolusindo.Business.PO.Queries;
+using GlobalSolusindo.DataAccess.PO.EntryForm;
+using GlobalSolusindo.Identity.User;
+using GlobalSolusindo.Identity.User.DML;
+using GlobalSolusindo.Identity.User.EntryForm;
+using GlobalSolusindo.Identity.User.Queries;
+using Kairos;
+using Kairos.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Transactions;
+using System.Web.Http;
+
+namespace GlobalSolusindo.Api.Controllers
+{
+    public class DashboardController : ApiControllerBase
+    {
+        [Route("dashboard/GetGoalCompletion")]
+        [HttpGet]
+        public IHttpActionResult GetGoalCompletion(string start, string end, int project)
+        {
+            //string accessType = "PO_ViewAll";
+            //ThrowIfUserHasNoRole(accessType);
+            //if (filter == null)
+            //    throw new KairosException("Missing search filter parameter");
+
+            DateTime startDate = Convert.ToDateTime(start);
+            DateTime endDate = Convert.ToDateTime(end);
+
+            using (var poQuery = new POQuery(Db))
+            {
+                var data = poQuery.GetDashboardGoalCompletion(startDate, endDate, project);
+                return Ok(new SuccessResponse(data));
+            }
+        }
+
+        [Route("dashboard/GetDashboardValue")]
+        [HttpGet]
+        public IHttpActionResult GetDashboardValue(string start, string end, int project)
+        {
+            //string accessType = "PO_ViewAll";
+            //ThrowIfUserHasNoRole(accessType);
+            //if (filter == null)
+            //    throw new KairosException("Missing search filter parameter");
+
+            DateTime startDate = Convert.ToDateTime(start);
+            DateTime endDate = Convert.ToDateTime(end);
+
+            using (var poQuery = new POQuery(Db))
+            {
+                var data = poQuery.GetDashboardValue(startDate, endDate, project);
+                return Ok(new SuccessResponse(data));
+            }
+        }
+        [Route("dashboard/GetRevenueCost")]
+        [HttpGet]
+        public IHttpActionResult GetRevenueCost(string start, string end, int project)
+        {
+            //string accessType = "PO_ViewAll";
+            //ThrowIfUserHasNoRole(accessType);
+            //if (filter == null)
+            //    throw new KairosException("Missing search filter parameter");
+
+            DateTime startDate = Convert.ToDateTime(start);
+            DateTime endDate = Convert.ToDateTime(end);
+
+            using (var poQuery = new POQuery(Db))
+            {
+                var data = poQuery.GetRevenueCost(startDate, endDate, project);
+                return Ok(new SuccessResponse(data));
+            }
+        }
+        [Route("dashboard/GetRevenueCostProfit")]
+        [HttpGet]
+        public IHttpActionResult GetRevenueCostProfit(string start, string end, int project)
+        {
+            //string accessType = "PO_ViewAll";
+            //ThrowIfUserHasNoRole(accessType);
+            //if (filter == null)
+            //    throw new KairosException("Missing search filter parameter");
+
+            DateTime startDate = Convert.ToDateTime(start);
+            DateTime endDate = Convert.ToDateTime(end);
+
+            using (var poQuery = new POQuery(Db))
+            {
+                var data = poQuery.GetRevenueCostProfit(startDate, endDate, project);
+                return Ok(new SuccessResponse(data));
+            }
+        }
+
+        [Route("dashboard/GetSalesReport")]
+        [HttpGet]
+        public IHttpActionResult GetSalesReport(string start, string end, int vendor)
+        {
+            //string accessType = "PO_ViewAll";
+            //ThrowIfUserHasNoRole(accessType);
+            //if (filter == null)
+            //    throw new KairosException("Missing search filter parameter");
+
+            DateTime startDate = Convert.ToDateTime(start);
+            DateTime endDate = Convert.ToDateTime(end);
+
+            using (var poQuery = new POQuery(Db))
+            {
+                var data = poQuery.GetSalesReport(startDate, endDate, vendor);
+                return Ok(new SuccessResponse(data));
+            }
+        }
+    }
+}
---
+++ GlobalSolusindo.Business/PO/RevenueCostProfit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalSolusindo.Business.PO
+{
+    public class RevenueCostProfit
+    {
+        public int? TotalRevenue { get; set; }
+        public int? TotalCost { get; set; }
+        public int? TotalProfit { get; set; }
+        public int? PersentaseRevenue { get; set; }
+        public int? PersentaseCost { get; set; }
+        public int? PersentaseProfit { get; set; }
+    }
+}
