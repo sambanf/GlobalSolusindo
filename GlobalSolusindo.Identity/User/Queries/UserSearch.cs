b7b80f231bdede0790f795a2d1fc3dca552d28a6
PERTAMINA\mk.indra.suryajaya <sambanugraha@gmail.com>
2019-08-15 14:35:19 +0700

    filter user by project

--- GlobalSolusindo.Identity/User/Queries/UserSearch.cs
+++ GlobalSolusindo.Identity/User/Queries/UserSearch.cs
@@ -25,6 +25,9 @@ namespace GlobalSolusindo.Identity.User.Queries
 
         [JsonProperty("kategoriJabatan_fk")]
         public int KategoriJabatan_FK { get; set; }
+
+        [JsonProperty("project_fk")]
+        public int project_fk{ get; set; }
     }
 
 
@@ -67,15 +70,13 @@ namespace GlobalSolusindo.Identity.User.Queries
                     || user.NoKTP.Contains(filter.Keyword)
                     || user.NoHP.Contains(filter.Keyword)
                     || user.Email.Contains(filter.Keyword)
-                    //|| user.Address.Contains(filter.Keyword)
-                    //|| user.Description.Contains(filter.Keyword)
                     );
 
             if ((UserKategoriJabatanFilter)filter.KategoriJabatan_FK != UserKategoriJabatanFilter.All)
             {
                 filteredRecords = filteredRecords
                     .Where(user =>
-                    user.KategoriJabatan_FK == (int)filter.KategoriJabatan_FK);
+                    user.KategoriJabatan_FK == (int)filter.KategoriJabatan_FK && user.Project == (int)filter.project_fk);
             }
 
             if (us.KategoriJabatan_FK == 7)
@@ -142,7 +143,7 @@ namespace GlobalSolusindo.Identity.User.Queries
             {
                 filteredRecords = filteredRecords
                     .Where(user =>
-                    user.KategoriJabatan_FK == (int)filter.KategoriJabatan_FK);
+                    user.KategoriJabatan_FK == (int)filter.KategoriJabatan_FK && user.Project == (int)filter.project_fk);
             }
 
             if (filter.User_PK > 0)
